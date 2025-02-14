bc77405c84261c523761b4e0481531270c04678d
RogueTMs <roguecyb@gmail.com>
2025-02-14 18:14:18 +0700

    First Solution
    Solve tasks 1 - 3.

--- src/Task1.hs
+++ src/Task1.hs
@@ -1,124 +1,50 @@
 {-# OPTIONS_GHC -Wall #-}
--- The above pragma enables all warnings
 
 module Task1 where
 
--- Explicit import of Prelude to hide functions
--- that are not supposed to be used in this assignment
-import Prelude hiding (reverse, map, filter, sum, foldl, foldr, length, head, tail, init, last, show, read)
-
------------------------------------
---
--- Checks whether the last digit is a valid check digit
--- for the rest of the given number using Luhn algorithm
---
--- Usage example:
---
--- >>> validate 3456
--- False
--- >>> validate 34561
--- True
--- >>> validate 34562
--- False
+import Prelude hiding
+  ( map,
+    reverse,
+    sum,
+  )
 
 validate :: Integer -> Bool
-validate = error "TODO: define validate"
-
------------------------------------
---
--- Computes check digit for given digits using Luhn algorithm
---
--- Usage example:
---
--- >>> luhn [3,4,5,6]
--- 1
+validate n =
+  let ds = toDigits n
+   in case ds of
+        [] -> False
+        _ ->
+          let checkDigit = last ds
+              rest = init ds
+           in luhn rest == checkDigit
 
 luhn :: [Int] -> Int
-luhn = error "TODO: define luhn"
-
------------------------------------
---
--- Produces list of digits for given positive number;
--- otherwise (for zero and negative numbers) returns empty list
---
--- Usage example:
---
--- >>> toDigits 3456
--- [3,4,5,6]
--- >>> toDigits 0
--- []
--- >>> toDigits (-123)
--- []
+luhn digits =
+  let s = sum (map normalize (doubleEveryOther (reverse digits)))
+   in (10 - (s `mod` 10)) `mod` 10
 
 toDigits :: Integer -> [Int]
-toDigits = error "TODO: define toDigits"
-
------------------------------------
---
--- Produces list in reverse order to the given one
---
--- Usage example:
---
--- >>> reverse "Hello"
--- "olleH"
--- >>> reverse [3,4,5,6]
--- [6,5,4,3]
+toDigits n
+  | n <= 0 = []
+  | otherwise = toDigits (n `div` 10) ++ [fromIntegral (n `mod` 10)]
 
 reverse :: [a] -> [a]
-reverse = error "TODO: define reverse"
-
------------------------------------
---
--- Doubles every other digit starting from first one
---
--- Usage example:
---
--- >>> doubleEveryOther [6,5,4,3]
--- [12,5,8,3]
+reverse [] = []
+reverse (x : xs) = reverse xs ++ [x]
 
 doubleEveryOther :: [Int] -> [Int]
-doubleEveryOther = error "TODO: define doubleEveryOther"
-
------------------------------------
---
--- Normalizes given number to single digit by subtracting 9
--- if it is greater than or equal to 10
---
--- (Assumes inputs between 0 and 18)
---
--- Usage example:
---
--- >>> normalize 12
--- 3
--- >>> normalize 1
--- 1
+doubleEveryOther [] = []
+doubleEveryOther [x] = [2 * x]
+doubleEveryOther (x : y : zs) = (2 * x) : y : doubleEveryOther zs
 
 normalize :: Int -> Int
-normalize = error "TODO: define normalize"
-
------------------------------------
---
--- Produces list with given function applied to each element
--- in given list
---
--- Usage example:
---
--- >>> map (\x -> x * 2) [1,2,3,4]
--- [2,4,6,8]
+normalize x
+  | x >= 10 = x - 9
+  | otherwise = x
 
 map :: (a -> b) -> [a] -> [b]
-map = error "TODO: define map"
-
------------------------------------
---
--- Computes sum of given list of numbers
---
--- Usage example:
---
--- >>> sum [3,8,5,3]
--- 19
--- >>> sum []
--- 0
+map _ [] = []
+map f (x : xs) = f x : map f xs
 
 sum :: [Int] -> Int
-sum = error "TODO: define sum"
+sum = foldr (+) 0
--- src/Task3.hs
+++ src/Task3.hs
@@ -1,21 +1,14 @@
 {-# OPTIONS_GHC -Wall #-}
--- Note: the above pragma enables all warnings
 
 module Task3 where
 
------------------------
--- Helper type synonyms
-
 type Peg = String
+
 type Move = (Peg, Peg)
 
------------------------
-
--- Usage examples
---
--- >>> hanoi 2 "a" "b" "c"
--- [("a","c"),("a","b"),("c","b")]
-
 hanoi :: Int -> Peg -> Peg -> Peg -> [Move]
-hanoi = error "TODO: define hanoi"
-
+hanoi 0 _ _ _ = []
+hanoi n fromPeg toPeg sparePeg =
+  hanoi (n - 1) fromPeg sparePeg toPeg
+    ++ [(fromPeg, toPeg)]
+    ++ hanoi (n - 1) sparePeg toPeg fromPeg
